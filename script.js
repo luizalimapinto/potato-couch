0c4482cf5cef5ee1f132b8efe9493e0946d86888
antoniapuspan <65085166+antoniapuspan@users.noreply.github.com>
2020-09-17 14:56:27 +0200

    names are fetched from database

--- script.js
+++ script.js
@@ -1,34 +1,29 @@
 //fetch data
-getData ()
+getData()
 
 function getData() {
 	fetch("movies-data.json")
-	.then(response => response.json())
-	.then(data => dataReceived(data));
+		.then(response => response.json())
+		.then(data => dataReceived(data));
 }
 
 function dataReceived(series) {
-    //loop through products
-    series.forEach(showSerie)
+	//loop through products
+	series.forEach(showSerie)
 }
 //executed once for each serie
 function showSerie(mySerie) {
-    console.log(mySerie) 
-    //finding the template
-const temp = document.querySelector("#seriesTemplate").content;
-    //clone the template
-    const myCopy = temp.cloneNode(true);
+	console.log(mySerie)
+	//finding the template
+	const temp = document.querySelector("#seriesTemplate").content;
+	//clone the template
+	const myCopy = temp.cloneNode(true);
 
-    //fill out the template
- mySerie   
-myCopy.querySelector(".data_name").textContet = mySerie.name 
+	//fill out the template
 
+	myCopy.querySelector(".data_name").textContent = mySerie.name;
 
- 
-
-
-
-//append
-const parentElem = document.querySelector("section#sci-fi");
-parentElem.appendChild(myCopy)
+	//append
+	const parentElem = document.querySelector("section#sci-fi");
+	parentElem.appendChild(myCopy)
 }
