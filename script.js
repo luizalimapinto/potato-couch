e24e9ebdbb76a07da21f28655ede2754e9557997
antoniapuspan <65085166+antoniapuspan@users.noreply.github.com>
2020-09-17 19:53:33 +0200

    fetched description

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 getData()
 
 function getData() {
-	fetch("movies-data.json")
+	fetch("database.json")
 		.then(response => response.json())
 		.then(data => dataReceived(data));
 }
@@ -10,6 +10,21 @@ function getData() {
 function dataReceived(series) {
 	//loop through products
 	series.forEach(showSerie)
+}
+
+function categoriesReceived(cats) {
+	createSections(cats);
+	fetchSeries();
+}
+
+function createSections(categories) {
+	categories.forEach(category => {
+		const section = document.createElement("section");
+		const h2 = document.createElement("h2");
+		h2.textContent = category;
+		section.appendChild(h2);
+		document.querySelector(".seriesList").appendChild(section);
+	})
 }
 //executed once for each serie
 function showSerie(mySerie) {
@@ -22,6 +37,15 @@ function showSerie(mySerie) {
 	//fill out the template
 
 	myCopy.querySelector(".data_name").textContent = mySerie.name;
+	myCopy.querySelector(".data_description").textContent = mySerie.description;
+
+	//setup classes for filtering
+	//1. find the elem
+	const article = myCopy.querySelector("article");
+	console.log("article");
+
+	//2. add classes
+	
 
 	//append
 	const parentElem = document.querySelector("section#sci-fi");
