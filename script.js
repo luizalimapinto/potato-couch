407dc56782e9dc9422d82de4715cabefe84ab0ff
antoniapuspan <65085166+antoniapuspan@users.noreply.github.com>
2020-09-17 22:36:39 +0200

    created sections
    created sections

--- script.js
+++ script.js
@@ -1,73 +1,75 @@
 //fetch data
-getData();
-const categoriesArray = [];
+getData()
+
+let categoriesArray = [];
 
 function getData() {
-	fetch("database.json")
-		.then(response => response.json())
-		.then(data => dataReceived(data));
+    fetch("database.json")
+        .then(response => response.json())
+        .then(data => dataReceived(data));
 }
 
 function dataReceived(series) {
-	createCategoriesArray(series);
-	//loop through products
-	series.forEach(showSerie)
+    //loop through products
+    createCategoriesArray(series);
+    createSections(categoriesArray);
+    series.forEach(showSerie)
 }
 
 function categoriesReceived(cats) {
-	createSections(cats);
-	fetchSeries();
+    fetchSeries();
 }
 
 function createCategoriesArray(data){
-	data.forEach(serie => {
-		const serieCategory = serie.category;
-		if (!categoriesArray.includes(serieCategory)){
-			categoriesArray.push(serieCategory)
-		}
-	})
-	console.log(categoriesArray)
+    data.forEach(serie => {
+        const serieCategory = serie.category;
+        if (!categoriesArray.includes(serieCategory)){
+            categoriesArray.push(serieCategory)
+        }
+    })
 }
-function createSections(categories) {
-	categories.forEach(category => {
-		const section = document.createElement("section");
-		const h2 = document.createElement("h2");
-		h2.textContent = category;
-		section.appendChild(h2);
-		document.querySelector(".seriesList").appendChild(section);
-	})
+
+function createSections(categories){
+    categories.forEach(category => {
+        const section = document.createElement("section");
+        section.setAttribute("id", category);
+        const h2 = document.createElement("h2");
+        h2.textContent = category;
+        section.appendChild(h2)
+        document.querySelector(".seriesList").appendChild(section)
+    })
 }
+
 //executed once for each serie
 function showSerie(mySerie) {
-	console.log(mySerie)
-	//finding the template
-	const temp = document.querySelector("#seriesTemplate").content;
-	//clone the template
-	const myCopy = temp.cloneNode(true);
-	
-	const imgName = "img/" + mySerie.image + ".jpg";
-	
-	const img = myCopy.querySelector(".series_image"); img.setAttribute("src", imgName)
-	
+    console.log(mySerie)
+    //finding the template
+    const temp = document.querySelector("#seriesTemplate").content;
+    //clone the template
+    const myCopy = temp.cloneNode(true);
+    
+    const imgName = "img/" + mySerie.image + ".jpg";
+    
+    const img = myCopy.querySelector(".series_image"); img.setAttribute("src", imgName)
+    
 
-	//fill out the template
+    //fill out the template
 
-	myCopy.querySelector(".data_name").textContent = mySerie.name;
-	myCopy.querySelector(".data_description").textContent = mySerie.description;
-	myCopy.querySelector(".started").textContent = mySerie.started;
-	myCopy.querySelector(".ended").textContent = mySerie.ended;
-	myCopy.querySelector(".data_episodesTotal span").textContent = mySerie.episodesTotal;
-	myCopy.querySelector(".data_seasons span").textContent = mySerie.seasons;
+    myCopy.querySelector(".data_name").textContent = mySerie.name;
+    myCopy.querySelector(".data_description").textContent = mySerie.description;
+    myCopy.querySelector(".started").textContent = mySerie.started;
+    myCopy.querySelector(".ended").textContent = mySerie.ended;
+    myCopy.querySelector(".data_episodesTotal span").textContent = mySerie.episodesTotal;
+    myCopy.querySelector(".data_seasons span").textContent = mySerie.seasons;
 
-	//setup classes for filtering
-	//1. find the elem
-	const article = myCopy.querySelector("article");
-	console.log("article");
+    //setup classes for filtering
+    //1. find the elem
+    const article = myCopy.querySelector("article");
+    console.log("article");
 
-	//2. add classes
-	
-
-	//append
-	const parentElem = document.querySelector("section#sci-fi");
-	parentElem.appendChild(myCopy)
+    //2. add classes
+    
+    let sectionID = "#" + mySerie.category
+    //append
+    document.querySelector(sectionID).appendChild(myCopy)
 }
